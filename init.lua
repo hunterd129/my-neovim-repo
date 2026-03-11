d7af108a7213c166d52f91c0d6a7324626bbf6ee
hunterd129 <hunterd129@icloud.com>
2026-03-11 17:40:06 -0600

    fixed the mistake of accidentally committing my entire .config. lol

---
+++ init.lua
@@ -0,0 +1,57 @@
+						--Neovim Config--
+--Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+--basic appearance--
+vim.o.number = true 
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.termguicolors = true
+
+--Keybindings
+require("keymaps")
+
+--Plugin theme--
+require("lazy").setup({
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				transparent_background = true, -- Native transparency handling
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end
+	},
+})
+
+--Syntax Hightlighting--
+
+vim.cmd('syntax enable')
+
+
+--status bar--
+
+vim.opt.laststatus = 3
+
+--transparency--
+
+local highlights = {
+    "Normal", "NormalNC", "SignColumn", "StatusLine", "StatusLineNC", "EndOfBuffer"
+}
+for _, group in ipairs(highlights) do
+    vim.api.nvim_set_hl(0, group, { bg = "NONE", ctermbg = "NONE" })
+end
