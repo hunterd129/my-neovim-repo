19757075aa91663da75fc8f151097dd92ebc81af
hunterd129 <hunterd129@icloud.com>
2026-03-12 10:10:23 -0600

    changes

--- init.lua
+++ init.lua
@@ -20,6 +20,12 @@ vim.o.cursorline = true
 vim.o.termguicolors = true
 vim.g.have_nerd_font = true
 
+--tab sizing
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
+
 --Keybindings
 require("keymaps")
 
