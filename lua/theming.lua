c7f0e048211cd0049cc034cc383c457093802277
hunterd129 <hunterd129@icloud.com>
2026-03-12 14:52:23 -0600

    added lualine plugin

--- lua/theming.lua
+++ lua/theming.lua
@@ -1,17 +1,32 @@
 -- Theming section
 
 -- catppuccin theme & transparency
-    require("lazy").setup({
-    	{
-    		"catppuccin/nvim",
-    		name = "catppuccin",
-    		priority = 1000,
-    		config = function()
-    			require("catppuccin").setup({
-    				flavour = "mocha",
-    				transparent_background = true, -- Native transparency handling
-			})
-    			vim.cmd.colorscheme("catppuccin")
-	    	end
-    	},
-    })
+require("lazy").setup({
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+                transparent_background = true, -- Native transparency handling
+            })
+            vim.cmd.colorscheme("catppuccin")
+        end
+    },
+    -- Lualine
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup({
+                options = {
+                    theme = 'catppuccin', 
+                    component_separators = { left = '', right = ''}, 
+                    section_separators = { left = '', right = ''},
+                    globalstatus = true,
+                }
+            })
+        end
+    },
+})
