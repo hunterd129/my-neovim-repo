424829d3c80b1587497140151246599687c8cefe
hunterd129 <hunterd129@icloud.com>
2026-03-12 11:01:46 -0600

    added custom keybinding for <Esc>

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,8 +7,11 @@
 
 --Sidebar explorer (Lex) with <Leader>e
     vim.keymap.set("n", "<leader>e", ":Lex 30<CR>")
+--normal mode toggle
+    vim.keymap.set("i", "jj", "<Esc>")
 
 --Netrw Aesthetic Tweaks
     vim.g.netrw_banner = 0		-- Hide help banner
     vim.g.netrw_winsize = 25	-- Side window width
     vim.g.netrw_liststyle = 3	-- Tree view
+
