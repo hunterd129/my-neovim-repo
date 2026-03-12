51c169a83a21bea4d465efa367911ab5927c2100
hunterd129 <hunterd129@icloud.com>
2026-03-11 19:37:28 -0600

    removed the theming section from init.lua to theming.lua in \lua

--- init.lua
+++ init.lua
@@ -23,20 +23,7 @@ vim.o.termguicolors = true
 require("keymaps")
 
 --Plugin theme--
-require("lazy").setup({
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-				transparent_background = true, -- Native transparency handling
-			})
-			vim.cmd.colorscheme("catppuccin")
-		end
-	},
-})
+require("theming")
 
 --Syntax Hightlighting--
 
---
+++ lua/theming.lua
@@ -0,0 +1,15 @@
+
+require("lazy").setup({
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				transparent_background = true, -- Native transparency handling
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end
+	},
+})
