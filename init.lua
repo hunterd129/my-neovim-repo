572fcbc3198a34e67a5ed9aa738572e5d184d2f1
hunterd129 <hunterd129@icloud.com>
2026-03-14 21:48:11 -0600

    modularization

--- init.lua
+++ init.lua
@@ -1,29 +1,10 @@
 						--Neovim Config--
---basic appearance--
-    vim.o.number = true 
-    vim.o.relativenumber = true
-    vim.o.cursorline = true
-    vim.o.termguicolors = true
-    vim.g.have_nerd_font = true
-    vim.opt.scrolloff = 8
 
---tab sizing--
-    vim.opt.tabstop = 4
-    vim.opt.shiftwidth = 4
-    vim.opt.expandtab = true
-    vim.opt.softtabstop = 4
-
+--Basic neovim settings--
+    require("basics")
 --Keybindings--
     require("keymaps")
 
 --Lazy nvim plugins--
     require("lazy-nvim")
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-
---Syntax Hightlighting--
-    vim.cmd('syntax enable')
-
---status bar--
-    vim.opt.laststatus = 3
 
