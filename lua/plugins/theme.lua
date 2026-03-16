3bc346dcac6ae4ba22e0aa06b2a8d730e1dd336e
hunterd129 <hunterd129@icloud.com>
2026-03-15 18:40:30 -0600

    swapped to tokyo night colour scheme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,27 +1,45 @@
 --Catppuccin theme & transparency
 return {
     {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                flavour = "mocha",
-                transparent_background = true,
-                integrations = {
-                    mini = {
-                        enabled = true,
-                        indentscope_color = "rosewater",
-                    },
-                },
-            })
-            vim.cmd.colorscheme("catppuccin")
-        end
+        --"catppuccin/nvim",
+        --name = "catppuccin",
+        --priority = 1000,
+        --config = function()
+            --require("catppuccin").setup({
+                --flavour = "mocha",
+                --transparent_background = true,
+                --integrations = {
+                    --mini = {
+                        --enabled = true,
+                        --indentscope_color = "rosewater",
+                    --},
+                --},
+            --})
+            --vim.cmd.colorscheme("catppuccin")
+        --end
     },
 --Smear cursor
     {
         "sphamba/smear-cursor.nvim",
         opts = {},
+    },
+
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("tokyonight").setup({
+                style = "night",
+                transparent = true,
+                styles = {
+                    sidebars = "dark",
+                    floats = "dark",
+                },
+            })
+            vim.cmd([[colorscheme tokyonight-night]])
+        end,
     }
+
 }
 
