672600f7985ead1d032d45c7d0b0775dc8cb4b49
hunterd129 <hunterd129@icloud.com>
2026-03-15 20:37:53 -0600

    catppuccin was commented out in an easier way using --[[ --]]

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,23 +1,27 @@
---Catppuccin theme & transparency
+--General appearance
 return {
+
+--Catppuccin theme & transparency
+--[[        
     {
-        --"catppuccin/nvim",
-        --name = "catppuccin",
-        --priority = 1000,
-        --config = function()
-            --require("catppuccin").setup({
-                --flavour = "mocha",
-                --transparent_background = true,
-                --integrations = {
-                    --mini = {
-                        --enabled = true,
-                        --indentscope_color = "rosewater",
-                    --},
-                --},
-            --})
-            --vim.cmd.colorscheme("catppuccin")
-        --end
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+                transparent_background = true,
+                integrations = {
+                    mini = {
+                        enabled = true,
+                        indentscope_color = "rosewater",
+                    },
+                },
+            })
+            vim.cmd.colorscheme("catppuccin")
+        end
     },
+--]]    
 --Smear cursor
     {
         "sphamba/smear-cursor.nvim",
