8f74a67f0890b3ad26ceb07e1ad470ac0b1bc278
hunterd129 <hunterd129@icloud.com>
2026-03-17 13:16:07 -0600

    replaced Snacks notifier function with Noice.nvim

---
+++ lua/plugins/Noice.lua
@@ -0,0 +1,24 @@
+return {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+    },
+    opts = {
+        lsp = {
+            hover = { enabled = false },
+            signature = { enabled = false },
+            message = { enabled = false },
+        },
+        presets = {
+            bottom_search = false,
+            command_palette = true,
+            long_message_to_split = true,
+        },
+    },
+}
+
+
+
+
