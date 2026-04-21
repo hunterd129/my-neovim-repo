dca3cf34b48e2c6dba56a5fab3b26413dc73e78c
hunterd129 <hunterd129@icloud.com>
2026-04-21 17:18:53 -0600

    things

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,30 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+--options
 vim.o.colorcolumn = "80"
 vim.opt.scrolloff = 10
 vim.opt.termguicolors = true
+vim.opt.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.termguicolors = true
+vim.g.have_nerd_font = true
+vim.opt.scrolloff = 10
+vim.cmd("syntax enable")
+vim.o.mouse = "a"
+vim.o.showmode = false
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.colorcolumn = "80"
+vim.schedule(function()
+	vim.o.clipboard = "unnamedplus"
+end)
+vim.o.breakindent = true
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+--Tab sizing--
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
